8806b3dbb16d8694fd9b6a88e16882e1c7a36a4a
piper-hyowon <hyowon09877@gmail.com>
2025-07-24 22:34:06 +0900

    CountDistinctSlices (go) 100%

--- lesson/15_CaterpillarMethod/CountDistinctSlices/main.go
+++ lesson/15_CaterpillarMethod/CountDistinctSlices/main.go
@@ -2,8 +2,6 @@
 
 package solution
 
-import "fmt"
-
 func Solution(M int, A []int) int {
 	if len(A) == 1 {
 		return 1
@@ -39,11 +37,9 @@ func Solution(M int, A []int) int {
 
 		// 다 유니크하면 애벌레 늘려
 		if isUnique {
-			fmt.Printf("(%d, %d) distinct!\n", tail, head)
 			cnt++
 			head++
 		} else {
-			fmt.Printf("(%d, %d) same!!\n", tail, head)
 			// 같으면 꼬리 앞으로 이동해서길이 줄여
 			tail++
 			head = tail
@@ -57,4 +53,46 @@ func Solution(M int, A []int) int {
 	return cnt
 }
 
-// (6,[1,5,1,5,2])
+// Task Score 100% Correctness 100% Performance 100%
+// 슬라이스 중복 매번 확인 X
+package solution
+
+func Solution(M int, A []int) int {
+	if len(A) == 1 {
+		return 1
+	}
+
+	const  MAX = 1_000_000_000
+	cnt := 0
+	// 애벌레
+	tail, head := 0, 0
+    set := make(map[int]bool) // 재사용! 꼬리 앞으로가면 delete
+
+	for {
+		if tail >= len(A) {
+			break
+		}
+		if head >= len(A) {
+            delete(set, A[tail])
+			tail++
+			continue
+		}
+
+		// 다 유니크하면 애벌레 늘려
+		if !set[A[head]] {
+            set[A[head]] = true
+            cnt += (head - tail + 1)
+			head++
+		} else {
+            delete(set, A[tail])
+			// 같으면 꼬리 앞으로 이동해서길이 줄여
+			tail++
+		}
+
+		if cnt >= MAX {
+			return MAX
+		}
+	}
+
+	return cnt
+}
